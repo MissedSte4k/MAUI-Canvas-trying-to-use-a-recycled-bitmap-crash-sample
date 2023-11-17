3b5237cfeeede1dbc22f9f94d7042a28c7cd4a79
MissedSte4k <36638029+MissedSte4k@users.noreply.github.com>
2023-11-17 12:56:32 +0200

    Slightly modified sample, to show the crashing behavior of images

---
+++ TestImages/AnimalTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestImages
+{
+	public class AnimalTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate OneAnimal { get; set; }
+		public DataTemplate MultipleAnimals { get; set; }
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			var animal = item as Animal;
+			
+			if (animal.Images.Count == 1)
+				return OneAnimal;
+			if (animal.Images.Count > 1)
+				return MultipleAnimals; 
+
+			return base.SelectTemplate(item, container);
+		}
+	}
+}
